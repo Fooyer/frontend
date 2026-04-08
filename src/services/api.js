6e809a93d243f319f0cd911571f847d556ce8c05
FreddyBaier499 <freddy.baier@bplus.com.br>
2026-04-07 23:49:27 -0300

    link oficial para middeware

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: "https://pp-back.fooyer.space/api",
   timeout: 30000,
 });
 
@@ -10,9 +10,9 @@ api.interceptors.response.use(
   (response) => response.data,
   (error) => {
     const message =
-      error.response?.data?.error || 'Erro inesperado. Tente novamente.';
+      error.response?.data?.error || "Erro inesperado. Tente novamente.";
     return Promise.reject(new Error(message));
-  }
+  },
 );
 
 export default api;
